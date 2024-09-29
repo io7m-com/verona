ecdc805e51d5950c0724da4c2715081734fce6c2
Mark Raynsford <code@io7m.com>
2024-09-29 20:10:25 +0000

    Use junit BOM.

--- com.io7m.verona.tests/src/main/java/module-info.java
+++ com.io7m.verona.tests/src/main/java/module-info.java
@@ -27,6 +27,7 @@ module com.io7m.verona.tests
   requires org.junit.jupiter.engine;
   requires org.junit.platform.commons;
   requires org.junit.platform.engine;
+  requires org.junit.platform.launcher;
 
   requires com.io7m.verona.core;
   requires net.jqwik.api;
